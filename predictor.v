b406566b7052a2d55977626377848b66a8015875
splenta222 <amirhoseinakbari202@gmail.com>
2023-06-01 10:26:31 +0330

    1

--- predictor.v
+++ predictor.v
@@ -1,5 +1,17 @@
 module predictor(input wire request, result, clk, taken, output reg prediction);
-
-// Your code
+reg [1:0]state = 2'b11;
+always @(posedge clk) begin
+    if (request) begin
+       prediction  = state[1]; 
+    end
+    if (result) begin
+        if (taken & state != 3) begin
+            state = state + 1;
+        end 
+        else if(!taken & state != 0) begin
+            state = state - 1;
+        end
+    end
+end
 
 endmodule
